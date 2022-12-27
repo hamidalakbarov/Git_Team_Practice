60b81266110fbcb63d35302b36fc4f3a14ce6287
latifaforozan <la.forozan@gmail.com>
2022-12-26 17:37:04 -0800

    Latifa updated a file

--- src/Latifa/Latifa.java
+++ src/Latifa/Latifa.java
@@ -5,6 +5,7 @@ public class Latifa {
     public static void main(String[] args) {
 
         System.out.println("NEVER EVER GIVE UP");
+        System.out.println("New line has been added");
 
     }
 }
