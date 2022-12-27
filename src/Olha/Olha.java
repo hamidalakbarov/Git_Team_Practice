53d63aaa8145baf58ca7d9754a661d54a84ccc26
olha <olhamelnychuk13@gmail.com>
2022-12-26 23:42:19 -0800

    adding some changes

--- src/Olha/Olha.java
+++ src/Olha/Olha.java
@@ -5,5 +5,10 @@ public class Olha {
     public static void main(String[] args) {
 
         System.out.println("Hello team! Olha is here.");
+
+        //adding some changes
+
+        System.out.println("Nice job everyone! Keep it up team.");
+
     }
 }
